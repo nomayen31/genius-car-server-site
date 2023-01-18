4b77fd970fec013d99544f191d789825a9f9f1e7
nomayen <nomayen.ohin@gmail.com>
2023-01-18 10:10:22 +0600

    checkout page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors =require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express()
 const port = process.env.PORT || 5000
 
@@ -14,12 +14,32 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.07lgbsy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+
+async function run() {
+    try {
+        const serviceCollection =client.db('geniusCar').collection('services');
+
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor =serviceCollection.find(query);
+            const services =await cursor.toArray();
+            res.send(services);
+        });
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service =await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) =>{
