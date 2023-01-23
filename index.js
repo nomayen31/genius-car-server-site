f74c4f46d51a2e6b044eb2afe54c3c5f3fa8b3fd
nomayen <nomayen.ohin@gmail.com>
2023-01-23 12:53:47 +0600

    order dlt added

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ async function run() {
     try {
         const serviceCollection =client.db('geniusCar').collection('services');
 
+        const orderCollection =client.db('geniusCar').collection('orders');
+
         app.get('/services', async(req, res) =>{
             const query = {}
             const cursor =serviceCollection.find(query);
@@ -31,6 +33,47 @@ async function run() {
             const query = {_id: ObjectId(id)};
             const service =await serviceCollection.findOne(query)
             res.send(service)
+        });
+
+        // orders API
+        app.get('/orders', async (req, res) => {
+            let query = {};
+
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set:{
+                    status: status
+                }
+            }
+            const result = await orderCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
         })
 
     }
